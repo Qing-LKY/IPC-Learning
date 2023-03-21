5f80bdb83ab866e4e6ee9f27ff09b3ae486328f8
lky <597572246@qq.com>
2023-03-21 16:55:05 +0000

    feat: base functions

---
+++ include/ds.h
@@ -0,0 +1,21 @@
+#ifndef DS_H
+#define DS_H
+
+#define EXIT_MSG 1
+#define SIZE 50
+
+#define DEF_PERM 0666
+
+typedef struct {
+    int msqid;
+    int ready;
+} status_t;
+
+typedef struct {
+    int mtype;
+    char mtext[SIZE];
+} msgbuf_t;
+
+typedef void (*sighandler_t)(int);
+
+#endif
--- src/libmsg.c
+++ src/libmsg.c
@@ -1,1 +1,68 @@
+#include "ds.h"
 #include "libmsg.h"
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+int gen_key(const char *path, key_t *key) {
+    *key = ftok(path, 'a');
+    if (errno != 0) return perror("ftok"), -1;
+    return 0;
+}
+
+int create_queue(key_t key) {
+    return msgget(key, IPC_EXCL | IPC_CREAT | DEF_PERM);
+}
+
+int remove_queue(int msqid) {
+    return msgctl(msqid, IPC_RMID, NULL);
+}
+
+int connect_queue(key_t key, int *msqid) {
+    int ret;
+    ret = msgget(key, IPC_EXCL);
+    if (ret == -1) {
+        puts("Exist IPC queue not found. Now try to create one.");
+        ret = create_queue(key);
+        if (ret == -1) {
+            perror("msgget");
+            return -1;
+        }
+        puts("Success to create queue!");
+    } else {
+        puts("Connect to an existing IPC queue!");
+    }
+    *msqid = ret;
+    printf("-- msqid: %d --\n", *msqid);
+    return 0;
+}
+
+int send_msg(int msqid, int type, const char *s) {
+    static msgbuf_t tmp;
+    int len = 0, re;
+    if (type != EXIT_MSG) {
+        len = strlen(s);
+        if (len >= SIZE) {
+            printf("Too long message, has been cut to %d bytes.\n", SIZE);
+            len = SIZE - 1;
+            memcpy(tmp.mtext, s, len);
+        } else strcpy(tmp.mtext, s);
+    }
+    tmp.mtype = type;
+    re = msgsnd(msqid, &tmp, len, IPC_NOWAIT);
+    if (re == -1) perror("msgsnd");
+    return re == -1 ? -1 : len;
+}
+
+int recv_msg(int msqid, int type, char *buf) {
+    int re;
+    re = msgrcv(msqid, buf, SIZE, type, IPC_NOWAIT);
+    // if (re == -1) {
+    //     if (errno == ENOMSG) printf("Message of type %d not found!\n", type);
+    //     else perror("msgrcv");
+    // }
+    return re;
+}
